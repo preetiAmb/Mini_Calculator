d53b1cdb1f64fbe35f629d99a489fb941c5e3eac
Preeti <35522305+preetiAmb@users.noreply.github.com>
2021-09-17 10:38:38 +0200

    Update Program.cs
    Added functionality

--- Mini-Calculator/Program.cs
+++ Mini-Calculator/Program.cs
@@ -6,7 +6,48 @@ namespace Mini_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1;
+            int num2;
+            string operand;
+            ConsoleKeyInfo status;
+            float answer;
+
+            while (true)
+            {
+                Console.Write("Please enter the first integer: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Please enter the second integer: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Please enter an operand (+, -, /, *): ");
+                operand = Console.ReadLine();
+
+                switch (operand)
+                {
+                    case "-": 
+                        answer = num1 - num2;
+                        break;
+                    case "+":
+                        answer = num1 + num2;
+                        break;
+                    case "/":
+                        answer = num1 / num2;
+                        break;
+                    case "*":
+                        answer = num1 * num2;
+                        break;
+                    default:
+                        answer = 0;
+                        break;
+                }
+                Console.WriteLine(num1.ToString() + " " + operand + " " + num2.ToString() + " = " + answer.ToString());
+                Console.WriteLine("\n\n Do You Want To Break (Y/y)");
+                status = Console.ReadKey();
+                if(status.Key==ConsoleKey.Y)
+                {
+                    break;
+                }
+                Console.Clear();
+            }
         }
     }
 }
